d64714867bb202f49c108a3e779552cf79f5e308
Amanjot Singh <am350894@dal.ca>
2023-02-25 15:54:20 -0400

    All Events and Event Details

--- digi-soul/src/views/AllEvents.js
+++ digi-soul/src/views/AllEvents.js
@@ -1,16 +1,40 @@
-import { Button, Container } from "@mui/material";
+import { Button, Container} from "@mui/material";
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import CenteredTabs from "../components/CenteredTabs";
+import EventCard from "../components/Card";
+import eventsData from "../data/events.json";
 
 function AllEvents() {
+  const events = eventsData.map((event) => {
+    return (
+      <EventCard
+        key={event.eventId}
+        id={event.eventId}
+        name={event.eventName}
+        imgurl={event.eventImage}
+        details={event.eventDetails}
+      />
+    );
+  });
+
   return (
-    <main>
-      <Container maxWidth="l" sx={{ display: "flex", alignItems: "center"}}>
+    <main sx={{marginTop:"100px"}}>
+      <Container maxWidth="l" sx={{ display: "flex", alignItems: "center" }}>
         <Button variant="contained" startIcon={<AddIcon />}>
           Create
         </Button>
         <CenteredTabs />
+      </Container>
+      <Container
+        sx={{
+          marginTop: "50px",
+          display: "grid",
+          gap: "50px 50px",
+          gridTemplateColumns: "repeat(3, 1fr)",
+        }}
+      >
+        {events}
       </Container>
     </main>
   );
--- digi-soul/src/views/EventDetails.js
+++ digi-soul/src/views/EventDetails.js
@@ -1,7 +1,48 @@
 import React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Container } from "@mui/system";
+import { useParams } from "react-router-dom";
+import eventsData from "../data/events.json";
 
 function EventDetails() {
-  return <h1>Event Details</h1>;
+  const { id } = useParams();
+  console.log(id);
+  const currentEvent = eventsData.filter((event) => event.eventId == id).at(0);
+
+  console.log(currentEvent);
+
+  return (
+    <Container sx={{ paddingTop: "50px" }}>
+      <Card sx={{ maxWidth: 600, margin: "0 auto" }}>
+        <CardMedia
+          sx={{ objectFit: "cover" }}
+          component="img"
+          alt="Event Image"
+          height="200"
+          image={currentEvent.eventImage}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h4" component="div">
+            {currentEvent.eventName}
+          </Typography>
+          <Typography variant="body1" color="text.secondary">
+            {currentEvent.eventDetailsLong}
+          </Typography>
+        </CardContent>
+        <CardActions>
+          <Button size="small" href={`/events`}>
+            Back
+          </Button>
+          <Button size="small">Save</Button>
+        </CardActions>
+      </Card>
+    </Container>
+  );
 }
 
 export default EventDetails;
