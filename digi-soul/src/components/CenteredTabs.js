76b16ccf497f15f8c863368c572159e394b02dc9
Amanjot Singh <am350894@dal.ca>
2023-02-26 11:02:43 -0400

    Fixed Tabs

--- digi-soul/src/components/CenteredTabs.js
+++ digi-soul/src/components/CenteredTabs.js
@@ -2,17 +2,20 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import { useLocation } from 'react-router-dom';
 
 export default function CenteredTabs() {
-  const [value, setValue] = React.useState(0);
+  const location = useLocation();
+  const [value, setValue] = React.useState(location.pathname.includes("all")?0:1);
 
   const handleChange = (event, newValue) => {
+    console.log(event)
     setValue(newValue);
   };
 
   return (
     <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
-      <Tabs value={value} onChange={handleChange} centered>
+      <Tabs value={value} centered>
         <Tab label="All Events" href='/events/all'/>
         <Tab label="Upcoming Events" href='/events/upcoming'/>
       </Tabs>
