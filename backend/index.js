bcb2a2e808bd9b6fe3ca5dcec0f322214378f6c1
Guru Jagata <gr421676@dal.ca>
2023-03-30 14:18:13 -0300

    code changes

--- backend/index.js
+++
@@ -1,9 +0,0 @@
-const http = require('http');
-const app = require("./app");
-
-const port = process.env.PORT || 3000
-const server = http.createServer(app);
-
-server.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
