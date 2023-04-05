6536256d00e5678ccc2dfbb7e480d4ad7d93974c
Amanjot Singh <am350894@dal.ca>
2023-04-04 21:16:26 -0300

    Delete Event

--- digi-soul/src/views/events/MyEvents.js
+++ digi-soul/src/views/events/MyEvents.js
@@ -2,9 +2,39 @@ import { Button, Container, Grid } from "@mui/material";
 import React from "react";
 import AddIcon from "@mui/icons-material/Add";
 import MyEventCard from "../../components/MyEventCard";
-import eventsData from "../../data/events.json";
+// import eventsData from "../../data/events.json";
+import { eventsApi } from "./services/events-api";
 
 function MyEvents() {
+  const [eventsData, setEventsData] = React.useState([]);
+
+  React.useEffect(() => {
+    eventsApi
+      .getAllEvents()
+      .then((res) => {
+        setEventsData(() => res?.data?.events || []);
+      })
+      .catch((err) => {
+        console.log("While fetching events-->", err);
+      });
+  }, []);
+
+  function deleteEvent(id) {
+    eventsApi
+      .removeEvent(id)
+      .then((res) => {
+        console.log("Event Delete-->", res);
+        setEventsData((prevEventData) => {
+          return prevEventData.filter((event) => {
+            return event._id !== id;
+          });
+        });
+      })
+      .catch((err) => {
+        console.log("While deleting event-->", err);
+      });
+  }
+
   return (
     <Container sx={{ marginTop: "25px" }}>
       <Container maxWidth="l">
@@ -21,11 +51,12 @@ function MyEvents() {
           {eventsData.map((event) => (
             <Grid item key={event.eventId} xs={12} sm={6} md={4}>
               <MyEventCard
-                key={event.eventId}
-                id={event.eventId}
-                name={event.eventName}
-                imgurl={event.eventImage}
-                details={event.eventDetails}
+                key={event._id}
+                id={event._id}
+                name={event.name}
+                imageURL={event.imageURL}
+                brief={event.brief}
+                deleteEvent={deleteEvent}
               />
             </Grid>
           ))}
